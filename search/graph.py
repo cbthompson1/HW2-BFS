b80b8c45fe2c4fcd55d68484e0891ef364ef27e1
Chris Thompson <cthompson4@pcd-77xx15j-lt.gladstone.internal>
2025-01-17 16:51:16 -0800

    HW2 initial run

--- search/graph.py
+++ search/graph.py
@@ -12,17 +12,53 @@ class Graph:
         """
         self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
 
+    def _return_path(self, breadcrumbs, start, end):
+        """
+        Generates path from start to end nodes.
+
+        Args:
+            breadcrumbs (dict): list of nodes to their previous nodes from start.
+            start (str): start node
+            end (str): end node
+        Returns:
+            List of nodes describing path from start to end using bfs method.
+        """
+        path = [end]
+        curr = end
+        while curr != start:
+            curr = breadcrumbs[curr]
+            path.append(curr)
+        return path[::-1]
+
     def bfs(self, start, end=None):
         """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
+        Performs a breadth first traversal / pathfinding on graph object.
 
-        * If there's no end node input, return a list nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, return None
-
+        Args:
+            start (string): Key of the node to start traversal / pathfinding.
+            end (string): Key of the goal node.
+        Returns:
+            List of nodes representing either a traversal of the connected
+            component of the graph or a path from the start to end node.
         """
-        return
+        queue = [start]
+        order = []
+        explored = set()
+        if end:
+            breadcrumbs = {start: ''}
 
-
-
-
+        while queue:
+            current_name = queue.pop(0)
+            if current_name in explored:
+                continue
+            explored.add(current_name)
+            if not end:
+                order.append(current_name)
+            current_node = self.graph[current_name]
+            for node in current_node.keys():
+                if end and node not in explored:
+                    breadcrumbs[node] = current_name
+                    if node == end:
+                        return self._return_path(breadcrumbs, start, end)
+                queue.append(node)
+        return order or None
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -2,25 +2,62 @@
 import pytest
 from search import graph
 
+def test_bfs_no_graph():
+    """No graph input should return KeyError."""
+    g = graph.Graph('test/test_data/empty.adjlist')
+    with pytest.raises(KeyError):
+        g.bfs('Empty Node')
+
+def test_bfs_unconnected_graph():
+    """An unconnected graph should not be fully traversable."""
+    g = graph.Graph('test/test_data/unconnected.adjlist')
+    assert g.bfs('1') == ['1', '2', '3', '4']
+    assert g.bfs('1', '6') == None
+
+def test_bfs_no_path_graph():
+    """A graph with no path from A to B should return None."""
+    g = graph.Graph('test/test_data/no_path.adjlist')
+    assert g.bfs('1', '12') == ['1', '2', '7', '9', '11', '12']
+    assert g.bfs('12', '1') == None
+
 def test_bfs_traversal():
     """
-    TODO: Write your unit test for a breadth-first
-    traversal here. Create an instance of your Graph class 
-    using the 'tiny_network.adjlist' file and assert 
-    that all nodes are being traversed (ie. returns 
-    the right number of nodes, in the right order, etc.)
+    Run breadth first traversal with tiny_network.adjlist. Confirm general
+    order.
     """
-    pass
+    g = graph.Graph('data/tiny_network.adjlist')
+    res = g.bfs('Steven Altschuler')
+
+    assert len(res) == 30 # Length matches.
+    assert len(set(res)) == 30 # No repeats.
+    assert res[1] == "32036252" # Only adj. node neighboring Steve's node.
+    assert res[2] == "Lani Wu" # Only adj. node neighboring 320... node.
+
+    # At this point divergence occurs between DFS and BFS, so confirm we only
+    # hit the paper ids (?) adjacent to Lani.
+    assert set(res[3:6]) == set(["30727954", "31806696", "32042149"])
+
 
 def test_bfs():
     """
-    TODO: Write your unit test for your breadth-first 
-    search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
-    a (shortest) path between them.
-    
-    Include an additional test for nodes that are not connected 
-    which should return None. 
+    Test BFS search and ensure:
+    1. A simple two step works well (Ryan -> Tony)
+    2. A farther reach also works (Ryan -> Hiten)
+    3. Unconnected people don't return results (Hao -> Tony)
     """
-    pass
+    g = graph.Graph('data/citation_network.adjlist')
+
+    ryan_to_tony = g.bfs('Ryan Corces', 'Tony Capra')
+    assert ryan_to_tony == ['Ryan Corces', '33046896', 'Tony Capra']
+
+    ryan_to_hiten = g.bfs('Ryan Corces', 'Hiten Madhani')
+    assert ryan_to_hiten == [
+        'Ryan Corces',
+        '31178118',
+        'Andrej Sali',
+        '31955845',
+        'Hiten Madhani',
+    ]
+
+    hao_to_tony = g.bfs("Hao Li", "Tony Capra")
+    assert hao_to_tony == None
